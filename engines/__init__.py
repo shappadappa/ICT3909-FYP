e6ec1e5eb3acc689b329a7fc1c26e835f74b462e
shappadappa <michael.farrugia5500@outlook.com>
2026-04-27 11:43:31 +0200

    add init file

--- engines/__init__.py
+++ engines/__init__.py
@@ -2,5 +2,24 @@ from engines.GAMealPlanner import GAMealPlanner
 from engines.LLMMealPlanner import LLMMealPlanner
 from engines.MealPlanner import MealPlanner
 from engines.RandomMealPlanner import RandomMealPlanner
+from engines.utils import (
+    get_ingredient,
+    get_pantry_ingredient,
+    load_all_ingredients,
+    make_pantry,
+    make_preferences,
+    make_recipe,
+)
 
-__all__ = ["MealPlanner", "GAMealPlanner", "LLMMealPlanner", "RandomMealPlanner"]
+__all__ = [
+    "MealPlanner",
+    "GAMealPlanner",
+    "LLMMealPlanner",
+    "RandomMealPlanner",
+    "load_all_ingredients",
+    "get_ingredient",
+    "get_pantry_ingredient",
+    "make_recipe",
+    "make_pantry",
+    "make_preferences",
+]
